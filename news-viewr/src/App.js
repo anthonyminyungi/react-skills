e4fe272edcb39b8c03c65fae1ecaeba67a8aa80c
anthonyminyungi <dbstnsdl12@naver.com>
2020-04-18 15:29:26 +0900

    Added prettierrc and newsapi

--- news-viewr/src/App.js
+++ news-viewr/src/App.js
@@ -4,10 +4,15 @@ import axios from 'axios';
 const App = () => {
   const [data, setData] = useState(null);
 
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then((res) => {
+  const onClick = async () => {
+    try {
+      const res = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=b2ff180d70a040bcbe02f259bceb3715',
+      );
       setData(res.data);
-    });
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
@@ -15,7 +20,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>불러오기</button>
       </div>
-      {data?.(
+      {data && (
         <textarea
           rows={7}
           value={JSON.stringify(data, null, 2)}
@@ -25,3 +30,5 @@ const App = () => {
     </div>
   );
 };
+
+export default App;
