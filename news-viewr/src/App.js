e6ffb95bef0230a75f46645e37126efc2d6a6e8c
anthonyminyungi <dbstnsdl12@naver.com>
2020-04-20 02:07:13 +0900

    #14 Complete to making simple page with using newsapi

--- news-viewr/src/App.js
+++ news-viewr/src/App.js
@@ -1,34 +1,14 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useCallback } from 'react';
+import { Route } from 'react-router-dom';
+
+import NewsPage from './pages/NewsPage';
+import Categories from './components/Categories';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-
-  const onClick = async () => {
-    try {
-      const res = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=b2ff180d70a040bcbe02f259bceb3715',
-      );
-      setData(res.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
---
+++ news-viewr/src/lib/usePromise.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+export default function usePromise(promiseCreator, deps) {
+  // 커스텀 Hook
+  const [loading, setLoading] = useState(false);
+  const [resolved, setResolved] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // API를 연동하여 개발할 때 절대 잊지 말아야 할 유의사항은
+    // useEffect에 등록하는 함수는 async로 작성하면 안되고
+    // 대신에 함수 내부에 async함수를 따로 만들어주고 호출하는 방식을 취해야 한다.
+    const process = async () => {
+      setLoading(true);
+      try {
+        const resolved = await promiseCreator();
+        setResolved(resolved);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+    process();
+  }, deps);
+  return [loading, resolved, error];
+}
